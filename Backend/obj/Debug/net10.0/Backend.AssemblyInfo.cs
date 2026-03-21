2942dfa890d2d6b9a4150783e30b49ce389130a2
Onthatile-Lesufi <231262@virtualwindow.co.za>
2026-03-21 21:47:01 +0200

    base -- Change obj references

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4a70de282170d13d75959175605caf923231d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4be378b96d21a12f016ff358c8a736e17064bf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
