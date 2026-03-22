267bb60f88f75d07685fad1c3cdb325533c831bb
Kai-Barker <241065@virtualwindow.co.za>
2026-03-22 20:46:35 +0200

    Completed models and relationships for the following:
    Branch
    BranchMaterials
    BranchProduct
    Material
    Order
    Product
    ProductMaterial

--- Backend/Models/Material.cs
+++ Backend/Models/Material.cs
@@ -2,6 +2,9 @@ namespace Artifax.Models
 {
     public class Material
     {
-        
+        public int MaterialID {get; set;}
+        public string MaterialName {get; set;}
+        public ICollection<BranchMaterial> BranchMaterials {get;set;}
+        public ICollection<ProductMaterial> ProductMaterials {get;set;}
     }
 }
