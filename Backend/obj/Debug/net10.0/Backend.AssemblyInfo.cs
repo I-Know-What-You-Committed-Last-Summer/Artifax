dd37c8b076cbc3a24725079e99b8ca5589039e72
Onthatile-Lesufi <231262@virtualwindow.co.za>
2026-03-22 20:54:30 +0200

    feat(frontend) - Create front end react act

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75ee7da5e1d358736b9183e816afef3ee7b4018c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9af4720d231410b20d8e0c54dfad88382d3e65ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
