9c052d5e9ead3e47fd3887bb9af0f90bdb8f47e0
Kai-Barker <241065@virtualwindow.co.za>
2026-04-14 12:45:01 +0200

    Added basic CRUD for product and product materials

---
+++ Backend/DTOs/ProductMaterialWriteDto.cs
@@ -0,0 +1,9 @@
+namespace Artifax.DTOs
+{
+    public class ProductMaterialWriteDto
+    {
+        public int ProductId { get; set; }
+        public int MaterialId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
