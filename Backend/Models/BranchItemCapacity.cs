c7d3ee1ad98652f1739f89a7922e5ea1eaa12325
Kai-Barker <241065@virtualwindow.co.za>
2026-04-29 16:07:14 +0200

    Made changes to the new models for relationships

---
+++ Backend/Models/BranchItemCapacity.cs
@@ -0,0 +1,12 @@
+namespace Artifax.Models
+{
+    public class BranchItemCapacity
+    {
+        public int BranchItemID { get; set; }
+        public int BranchID { get; set; } //FK
+        public int ItemID { get; set; } //FK
+        public int ItemQuantity { get; set; }
+        public Item Item {get; set;}
+        public Branch Branch {get; set;}
+    }
+}
