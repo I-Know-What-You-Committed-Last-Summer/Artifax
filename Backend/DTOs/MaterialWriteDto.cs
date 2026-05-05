251538d882db79715ef16e737af846526934a9cd
Kai-Barker <241065@virtualwindow.co.za>
2026-05-05 20:41:14 +0200

    Deleted old files

--- Backend/DTOs/MaterialWriteDto.cs
+++
@@ -1,9 +0,0 @@
-// namespace Artifax.DTOs
-// {
-//     //DTO excluding properties for sql relationships
-//     public class MaterialWriteDto
-//     {
-//        public string MaterialName {get; set;}
-//        public string MaterialImageURL {get; set;}
-//     }
-// }
