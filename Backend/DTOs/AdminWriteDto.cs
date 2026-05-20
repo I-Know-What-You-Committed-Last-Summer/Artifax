5e42088b18fecb7153494496835640f38829488f
Onthatile-Lesufi <231262@virtualwindow.co.za>
2026-05-20 17:13:51 +0200

    fix(backend) - Remove Admin Model

--- Backend/DTOs/AdminWriteDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Artifax.DTOs
-{
-    public class AdminWriteDto
-    {
-        public string AdminEmail { get; set; }
-        public string AdminName { get; set; }
-        public string AdminPassword { get; set; }
-    }
-}
