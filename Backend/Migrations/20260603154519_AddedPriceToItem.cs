4a72d128977a166930e133a0540015fcc9ab286b
Kai Barker <160474320+Kai-Barker@users.noreply.github.com>
2026-06-03 17:48:04 +0200

    Migrated price to database

---
+++ Backend/Migrations/20260603154519_AddedPriceToItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPriceToItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<float>(
+                name: "Price",
+                table: "Items",
+                type: "real",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Items");
+        }
+    }
+}
